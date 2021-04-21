5fa51bc8cdd0cebe5f5f58a56c7b343c4e4fac7b
t0shi <t0shiiii@icloud.com>
2021-04-21 05:04:26 -0700

    adding my keymap

--- keyboards/crkbd/keymaps/t0shiii/config.h
+++ keyboards/crkbd/keymaps/t0shiii/config.h
@@ -43,6 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 150
+#define TAPPING_TERM 180
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
